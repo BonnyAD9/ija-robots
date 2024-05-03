0c8f44716bfa4b06c13ae12908269a70d92349d4
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-05-03 10:51:04 +0200

    Add auto robot fields

--- src/main/java/ija/robots/ReditMenu.java
+++ src/main/java/ija/robots/ReditMenu.java
@@ -1,6 +1,5 @@
 package ija.robots;
 
-import java.util.ResourceBundle.Control;
 import java.util.function.BiConsumer;
 
 import ija.robots.actors.AutoRobot;
@@ -43,6 +42,10 @@ public class ReditMenu {
 
     private HBox crobot;
     private TextField rspeed;
+
+    private HBox arobot;
+    private TextField edist;
+    private TextField rdist;
 
     private SimHandler<SimObj> onRemove = null;
     private BiConsumer<Robot, Robot> onChangeRobot = null;
@@ -105,6 +108,7 @@ public class ReditMenu {
 
         robot.setVisible(true);
         crobot.setVisible(false);
+        arobot.setVisible(false);
 
         rtype.getSelectionModel().select(getRobotType());
         speed.setText(String.format("%.2f", r.speed()));
@@ -116,8 +120,11 @@ public class ReditMenu {
             rspeed.setText(String.format("%.2f", cr.rspeed() / Math.PI * 180));
         } else if (r instanceof AutoRobot ar) {
             crobot.setVisible(true);
+            arobot.setVisible(true);
 
             rspeed.setText(String.format("%.2f", ar.rspeed() / Math.PI * 180));
+            edist.setText(String.format("%.2f", ar.edist()));
+            rdist.setText(String.format("%.2f", -ar.erot() / Math.PI * 180));
         }
     }
 
@@ -175,7 +182,8 @@ public class ReditMenu {
             speed(),
             new Label("angle:"),
             angle(),
-            crobot()
+            crobot(),
+            arobot()
         );
         robot.setPadding(new Insets(0, 0, 0, 5));
         robot.setAlignment(Pos.CENTER_LEFT);
@@ -244,6 +252,38 @@ public class ReditMenu {
             Robot.class
         );
         return rspeed;
+    }
+
+    private HBox arobot() {
+        arobot = new HBox(
+            5,
+            new Label("d. dist:"),
+            edist(),
+            new Label("r. dist:"),
+            rdist()
+        );
+        arobot.setAlignment(Pos.CENTER_LEFT);
+        return arobot;
+    }
+
+    private TextField edist() {
+        edist = new TextField();
+        edist.setPrefWidth(60);
+        edist.setTextFormatter(numberFormatter(0, Double.MAX_VALUE));
+        setNumber(edist, (ed, r) -> r.edist(ed), AutoRobot.class);
+        return edist;
+    }
+
+    private TextField rdist() {
+        rdist = new TextField();
+        rdist.setPrefWidth(60);
+        rdist.setTextFormatter(numberFormatter(-360, 360));
+        setNumber(
+            rdist,
+            (rd, r) -> r.erot(-rd / 180 * Math.PI),
+            AutoRobot.class
+        );
+        return rdist;
     }
 
     private TextFormatter<?> numberFormatter(double min, double max) {
